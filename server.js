9936de2f05c243b4d52237c1b11c4e6ce4e40d58
SGK112 <143036545+SGK112@users.noreply.github.com>
2025-05-21 23:19:43 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import path from 'path';
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const path = require('path');
 
 dotenv.config();
 
