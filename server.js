1f30d4059f4dd91603cc39b90be2678359efc81e
SGK112 <143036545+SGK112@users.noreply.github.com>
2025-05-26 15:13:23 -0700

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const { parse } = require('csv-parse/sync');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors({ origin: process.env.CORS_ORIGIN || '*' }));
 app.use(express.json());
